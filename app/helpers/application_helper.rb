f109bd0016a5d10df98677746e9a8286094159a8
Tim Chapin <chapin.timothys@gmail.com>
2023-07-29 17:53:25 +0900

    Removed footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
+require 'net/http'
+require 'json'
 module ApplicationHelper
+  params = {
+    api_key: "APIKEY",
+    query: "QUERY",
+    country_code: "COUNTRY_CODE",
+    tld: "TLD"
+  }
+  uri = URI('https://api.scraperapi.com/structured/google/search')
+  uri.query = URI.encode_www_form(params)
+  website_content = Net::HTTP.get(uri)
+  print(website_content)
 end
