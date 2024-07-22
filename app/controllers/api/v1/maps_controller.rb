9abe531b6608fc4c282ad5b47fa56c23377d38c6
Tim Chapin <chapin.timothys@gmail.com>
2024-07-22 12:44:55 +0900

    Remove backend API call for MapBox API key. NOT SAFE METHOD

--- app/controllers/api/v1/maps_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::V1::MapsController < ApplicationController
-  before_action :authenticate_user!, except: [:api_key]
-
-  def api_key
-    render json: { api_key: ENV['MAPBOX_API_KEY'] }
-  end
-end
