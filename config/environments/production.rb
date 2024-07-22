9e0389bed7faa79cc65bd6fba8a8decbb8a4f71f
Tim Chapin <chapin.timothys@gmail.com>
2024-07-22 13:51:19 +0900

    Added application.css to assets precomp in produc config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,10 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  # Use this for cache busting. Bassically so that the users browser will always load a new css
+  config.assets.digest = true
+  # add the css file to the assets precompile pipeline
+  config.assets.precompile += %w( application.css )
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
